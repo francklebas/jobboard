f8a030d43dbddbe6ec40f3c0fdb342b1c2f782ca
francklebas <francklebas@ik.me>
2026-03-08 18:27:55 +0100

    feat: add Caddy reverse proxy with auto HTTPS for frontend and API

--- infra/main.tf
+++ infra/main.tf
@@ -37,6 +37,22 @@ resource "aws_security_group" "jobboard" {
   description = "Jobboard - SSH, API, Frontend"
 
   ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
